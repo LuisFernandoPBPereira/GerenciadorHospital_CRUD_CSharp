efda1a357b753a4ffa39d3b070ad1cbef33d559e
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-03-28 11:49:36 -0300

    Validações DataAnnotations login e cadastro

--- GerenciadorHospital/Dto/LoginRequestDto.cs
+++ GerenciadorHospital/Dto/LoginRequestDto.cs
@@ -5,8 +5,10 @@ namespace GerenciadorHospital.Dto
     public class LoginRequestDto
     {
         [Required]
+        [MinLength(Consts.TamanhoMinimoUserName, ErrorMessage = Consts.ErroDeValidacaoTamanhoUserName)]
         public string UserName { get; set; }
         [Required]
+        [RegularExpression(Consts.RegexSenha, ErrorMessage = Consts.ErroDeValidacaoSenha)]
         public string? Senha { get; set; }
     }
 }
