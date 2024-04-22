f68446643ffba0478d605dc547fffd34535822f9
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-04-22 17:52:16 -0300

    Adicionando um novo campo na model Médico

--- GerenciadorHospital/Models/MedicoModel.cs
+++ GerenciadorHospital/Models/MedicoModel.cs
@@ -1,10 +1,15 @@
-﻿namespace GerenciadorHospital.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GerenciadorHospital.Models
 {
     public class MedicoModel
     {
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
+        [NotMapped]
+        public IFormFile? Doc { get; set; }
+        public string? CaminhoDoc { get; set; }
         public string Senha { get; set; }
         public string Endereco { get; set; }
         public DateTime DataNasc { get; set; }
