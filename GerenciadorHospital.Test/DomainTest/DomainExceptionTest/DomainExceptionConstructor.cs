d27cd2cc481925060a9fe2248b3c7ce495780b72
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-06-27 15:46:09 -0300

    test: Testando construtor da exceção de domínio

---
+++ GerenciadorHospital.Test/DomainTest/DomainExceptionTest/DomainExceptionConstructor.cs
@@ -0,0 +1,23 @@
+﻿using GerenciadorHospital.Domain.Exceptions;
+
+namespace GerenciadorHospital.Test.DomainTest.DomainExceptionTest;
+
+public class DomainExceptionConstructor
+{
+    [Fact]
+    public void QuandoConstrutorValidoMensagemDeveSerAtribuida()
+    {
+        string mensagem = "mensagem teste";
+        DomainException domainException = new DomainException(mensagem);
+
+        Assert.Equal(mensagem, domainException.Mensagem);
+    }
+    
+    [Fact]
+    public void QuandoConstrutorVazioMensagemDeveSerAtribuida()
+    {
+        DomainException domainException = new DomainException();
+
+        Assert.NotNull(domainException.Mensagem);
+    }
+}
