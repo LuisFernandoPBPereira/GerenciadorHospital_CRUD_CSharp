e145f2487c92c9232b658ba08ea04248ad850aec
LuisFernandoPBPereira <luisfernandopbp@gmail.com>
2024-06-28 09:55:01 -0300

    feat: Configurando contexto do banco de dados

---
+++ GerenciadorHospital.Infraestructure/Data/Context/DatabaseContext.cs
@@ -0,0 +1,25 @@
+﻿using GerenciadorHospital.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorHospital.Infraestructure.Data.Context;
+
+public class DatabaseContext : DbContext
+{
+    public DatabaseContext(DbContextOptions options) : base(options){}
+
+    #region DbSet das Models
+    public DbSet<PacienteModel> Pacientes { get; set; }
+    public DbSet<MedicoModel> Medicos { get; set; }
+    public DbSet<ConvenioModel> Convenios { get; set; }
+    public DbSet<TipoExameModel> TiposExames { get; set; }
+    public DbSet<RegistroConsultaModel> RegistrosConsultas { get; set; }
+    public DbSet<LaudoModel> Laudos { get; set; }
+    public DbSet<MedicamentoPacienteModel> MedicamentosPaciente { get; set; }
+    public DbSet<UsuarioModel> Usuarios { get; set; }
+    #endregion
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
---
+++ GerenciadorHospital.Infraestructure/Data/ORM/EntityFrameworkORM.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GerenciadorHospital.Infraestructure.Data.ORM;
+
+public class EntityFrameworkORM<T> : IRepositorioORM<T> where T : class
+{
+    private readonly DbContext _context;
+    private readonly DbSet<T> _dbSet;
+
+    public EntityFrameworkORM(DbContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<T>();
+    }
+
+    public async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<T> GetByIdAsync(int id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public async Task UpdateAsync(T entity)
+    {
+        _dbSet.Attach(entity);
+        _context.Entry(entity).State = EntityState.Modified;
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        T entity = await GetByIdAsync(id);
+        if (entity != null)
+        {
+            _dbSet.Remove(entity);
+        }
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
